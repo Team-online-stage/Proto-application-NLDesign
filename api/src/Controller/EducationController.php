fc6a91ca2265c7e83167b08e91de5c46db09ee5f
Wilco Louwerse <wilco@conduction.nl>
2020-07-27 16:21:34 +0200

    Stage, big update for overzicht page and you can apply for a course now
    and some layout changes/fixes

--- api/src/Controller/EducationController.php
+++ api/src/Controller/EducationController.php
@@ -152,6 +152,38 @@ class EducationController extends AbstractController
         $variables['course'] = $commonGroundService->getResource(['component' => 'edu', 'type' => 'courses', 'id' => $id], $variables['query']);
         $variables['resources'] = $commonGroundService->getResource(['component' => 'edu', 'type' => 'courses'], $variables['query'])['hydra:member'];
 
+        // Lets see if there is a post to procces
+        if ($request->isMethod('POST')) {
+            $resource = $request->request->all();
+
+            //check if this user is already a participant
+            $participants = $commonGroundService->getResourceList(['component' => 'edu', 'type' => 'participants'], ['person' => $variables['user']['@id']])['hydra:member'];
+
+            $oldParticipant = [];
+            $participant = [];
+            $participant['courses'] = [];
+            if (count($participants) > 0) { //if this user is already a participant
+                //get the courses from this participant
+                $oldParticipant = $participants[0];
+                foreach ($oldParticipant['courses'] as $course) {
+                    array_push($participant['courses'], $course['@id']);
+                }
+            }
+
+            //add this course to the participant
+            array_push($participant['courses'], $variables['course']['@id']);
+
+            //update the existing participant
+            if (array_key_exists('@id', $oldParticipant)) {
+                $commonGroundService->updateResource($participant, $oldParticipant['@id']);
+            } else { //or if this user isn't a participant yet, create one
+                $participant['person'] = $variables['user']['@id'];
+                $commonGroundService->createResource($participant, ['component' => 'edu', 'type' => 'participants']);
+            }
+
+            return $this->redirectToRoute('app_education_course', ['id' => $variables['course']['id']]);
+        }
+
         return $variables;
     }
 
