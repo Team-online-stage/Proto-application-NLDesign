a0d4565739d5bce109561f21b8c94aba14e14b4a
Ruben van der Linde <ruben@conduction.nl>
2020-08-26 07:16:56 +0200

    Small  code nodes

--- api/src/Controller/ChinController.php
+++ api/src/Controller/ChinController.php
@@ -91,6 +91,11 @@ class ChinController extends AbstractController
             }
         }
 
+        // Alleen afgaan bij post EN ingelogde gebruiker
+        if ($request->isMethod('POST') ) {
+            // Check in object aanmaken en gebruiker doorsturen naar zijn checkin overzicht
+        }
+
         return $variables;
     }
 }
