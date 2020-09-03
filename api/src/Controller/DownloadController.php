051fe265d8ae46a52ce1e217614fea7fbd119901
Yorick Groeneveld <yorickgroeneveld@hotmail.com>
2020-09-03 14:12:29 +0200

    Fixed issue in CI style

--- api/src/Controller/DownloadController.php
+++ api/src/Controller/DownloadController.php
@@ -14,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * The Request test handles any calls that have not been picked up by another test, and wel try to handle the slug based against the wrc.
