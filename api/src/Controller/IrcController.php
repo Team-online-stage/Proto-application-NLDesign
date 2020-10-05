a0ef6475d61ba5d1fc5ab3749f8848624fac3111
Barry Brands <barrybrands02@hotmail.com>
2020-10-05 15:33:08 +0200

    IrcController added

--- api/src/Controller/IrcController.php
+++ api/src/Controller/IrcController.php
@@ -4,7 +4,7 @@
 
 namespace App\Controller;
 
-use App\Command\PubliccodeCommand;
+//use App\Command\PubliccodeCommand;
 use App\Service\ApplicationService;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,19 +15,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 
 /**
- * Class DeveloperController
+ * Class IrcController
  * @package App\Controller
  * @Route("/irc")
  */
 class IrcController extends AbstractController
 {
 
-//	/**
-//	 * @Route("/")
-//	 * @Template
-//	 */
-//	public function indexAction(CommonGroundService $commonGroundService, ApplicationService $applicationService)
-//	{
+//    /**
+//     * @Route("/")
+//     * @Template
+//     */
+//    public function indexAction(CommonGroundService $commonGroundService, ApplicationService $applicationService)
+//    {
+//        $variables = [];
+//
 //	    $variables = $applicationService->getVariables();
 //		// Moeten we ophalen uit de ingelogde sessie
 //		$person = $variables['user']['burgerservicenummer'];
@@ -35,35 +37,37 @@ class IrcController extends AbstractController
 //
 //		$variables['assents'] = $commonGroundService->getResourceList($defaultIrc, ['person'=>$person])['hydra:member'];
 //
-//		return $variables;
-//	}
+//        return $variables;
+//    }
 
     /**
-     * @Route("assents/{id}")
+     * @Route("/assents/{id}")
      * @Template
      */
     public function assentAction($id, CommonGroundService $commonGroundService, ApplicationService $applicationService, Request $request)
     {
-        $variables = $applicationService->getVariables();
+        $variables = [];
+        $variables['assent'] = $commonGroundService->getResource(['component' => 'irc', 'type' => 'assents', 'id' => $id]);
 
-        // We need need to get the assssent from a differend than standard location
+        // We need need to get the assent from a different than standard location
+        if (!empty($this->getUser())) {
+            $defaultIrc = "https://irc.huwelijksplanner.online/assents/";
+            $variables['user'] = $commonGroundService->getResource($this->getUser()->getPerson());
+            $variables['requester'] = $commonGroundService->getResource($variables['assent']['request']);
+            $update = false;
+            if (!key_exists('person', $variables['assent']) || $variables['assent']['person'] == null) {
+                $variables['assent']['person'] = $variables['user']['burgerservicenummer'];
+                $update = true;
+            }
+            if ($request->isMethod('POST') && $request->request->has('status')) {
+                $variables['assent']['status'] = $request->request->get('status');
 
-        $defaultIrc = "https://irc.huwelijksplanner.online/assents/";
-        $variables['assent'] = $commonGroundService->getResource($defaultIrc . $id);
-        $variables['requester'] = $commonGroundService->getResource(['component' => 'vrc', 'type' => 'requests', 'id' => $variables['assent']['request']])['submitters'][0]['person'];
-        $update = false;
-        if (!key_exists('person', $variables['assent']) || $variables['assent']['person'] == null) {
-            $variables['assent']['person'] = $variables['user']['burgerservicenummer'];
-            $update = true;
-        }
-        if ($request->isMethod('POST') && $request->request->has('status')) {
-            $variables['assent']['status'] = $request->request->get('status');
-
-            $update = true;
-//            $this->addFlash('success', "assent has been updated with status {$variables["assent"]["status"]}");
-        }
-        if ($update) {
-            $variables['assent'] = $commonGroundService->saveResource($variables['assent']);
+                $update = true;
+                $this->addFlash('success', "assent has been updated with status {$variables["assent"]["status"]}");
+            }
+            if ($update) {
+                $variables['assent'] = $commonGroundService->saveResource($variables['assent'], ['component' => 'irc', 'type' => 'assents']);
+            }
         }
 
         return $variables;
@@ -73,31 +77,17 @@ class IrcController extends AbstractController
      * @Route("/assents")
      * @Template
      */
-    public function assentsAction($id, CommonGroundService $commonGroundService, ApplicationService $applicationService, Request $request)
+    public function assentsAction(CommonGroundService $commonGroundService, ApplicationService $applicationService, Request $request)
     {
-        $variables = $applicationService->getVariables();
-//
-//		// We need need to get the assssent from a differend than standard location
-//
-//		$defaultIrc = "https://irc.huwelijksplanner.online/assents/";
-//        $variables['assent'] = $commonGroundService->getResource($defaultIrc . $id);
-//        $variables['requester'] = $commonGroundService->getResource(['component'=>'vrc', 'type'=>'requests','id'=>$variables['assent']['request']])['submitters'][0]['person'];
-//        $update = false;
-//        if(!key_exists('person', $variables['assent']) || $variables['assent']['person'] == null){
-//            $variables['assent']['person'] = $variables['user']['burgerservicenummer'];
-//            $update = true;
-//        }
-//        if($request->isMethod('POST') && $request->request->has('status')){
-//            $variables['assent']['status'] = $request->request->get('status');
-//
-//            $update = true;
-////            $this->addFlash('success', "assent has been updated with status {$variables["assent"]["status"]}");
-//        }
-//        if($update){
-//            $variables['assent'] = $commonGroundService->saveResource($variables['assent']);
-//
-//
-//        }
+        $variables = [];
+
+        if (!empty($this->getUser())) {
+            $variables['user'] = $commonGroundService->getResource($this->getUser()->getPerson());
+            $user['person@id'] = $this->getUser()->getPerson();
+            $variables['assents'] = $commonGroundService->getResourceList(['component' => 'irc', 'type' => 'assents'], ['person' => $variables['user']['burgerservicenummer'],])['hydra:member'];
+
+        }
+
         return $variables;
     }
 }
