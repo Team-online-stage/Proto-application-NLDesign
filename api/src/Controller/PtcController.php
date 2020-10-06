a4565f72bb8f84ff802b9341c5f5215c55604e76
Ruben van der Linde <ruben@conduction.nl>
2020-10-06 12:45:42 +0200

    Fix op onterecht processen leeg gooien

--- api/src/Controller/PtcController.php
+++ api/src/Controller/PtcController.php
@@ -90,9 +90,9 @@ class PtcController extends AbstractController
         if ($loadrequest = $request->query->get('request')) {
             $requestUUID = $commonGroundService->getUuidFromUrl($loadrequest);
             $variables['request'] = $commonGroundService->getResource(['component'=>'vrc', 'type'=>'requests', 'id'=>$requestUUID]);
+            $variables['submit'] = 'true';
             $session->set('request', $variables['request']);
 
-            var_dump($variables['request']);
         }
 
         $variables['process'] = $commonGroundService->getResource(['component' => 'ptc', 'type' => 'process_types', 'id' => $id]);
@@ -105,14 +105,12 @@ class PtcController extends AbstractController
             $session->remove('request');
         }
 
-        //if(!array_key_exists('request', $variables)){
-            $variables['request'] = $session->get('request', ['requestType'=>$variables['process']['requestType'], 'properties'=>[]]);
+        $variables['request'] = $session->get('request', ['requestType'=>$variables['process']['requestType'], 'properties'=>[]]);
 
-        //}
 
         // What if the request in session is defrend then the procces type that we are currently running? Or if we dont have a process_type at all? Then we create a base request
         if (
-            (array_key_exists('processType', $variables['request']) && $variables['request']['processType'] != $variables['process']['@id'])
+            (array_key_exists('processType', $variables['request']) && $commonGroundService->getUuidFromUrl($variables['request']['processType']) != $variables['process']['id'])
             ||
             !array_key_exists('processType', $variables['request'])
         ) {
@@ -123,7 +121,6 @@ class PtcController extends AbstractController
             $variables['request']['requestType'] = $variables['process']['requestType'];
             $session->set('request', $variables['request']);
 
-            var_dump('procces leeggegoid');
         }
 
         // lets handle a current stage
@@ -194,8 +191,6 @@ class PtcController extends AbstractController
             // stores an attribute in the session for later reuse
             $variables['request'] = $request;
 
-            var_dump($variables['request']);
-            
             $session->set('request', $request);
         }
 
