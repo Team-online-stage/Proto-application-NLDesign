7d8b219b8cf0466fed87c803d17b841ad5e9ba85
Barry Brands <barrybrands02@hotmail.com>
2020-10-08 10:15:19 +0200

    Bugfix

--- api/src/Controller/IrcController.php
+++ api/src/Controller/IrcController.php
@@ -48,12 +48,14 @@ class IrcController extends AbstractController
     {
         $variables = [];
         $variables['assent'] = $commonGroundService->getResource(['component' => 'irc', 'type' => 'assents', 'id' => $id]);
-
+        
         // We need need to get the assent from a different than standard location
         if (!empty($this->getUser())) {
             $defaultIrc = "https://irc.huwelijksplanner.online/assents/";
             $variables['user'] = $commonGroundService->getResource($this->getUser()->getPerson());
-            $variables['requester'] = $commonGroundService->getResource($variables['assent']['request']);
+            if(!empty($variables['assent']['requester'])) {
+                $variables['requester'] = $commonGroundService->getResource($variables['assent']['requester']);
+            }
             $update = false;
             if (!key_exists('person', $variables['assent']) || $variables['assent']['person'] == null) {
                 $variables['assent']['person'] = $variables['user']['burgerservicenummer'];
