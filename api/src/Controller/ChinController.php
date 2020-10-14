6d517772e305cc20d69a8a62526e3e5198429f50
ginokok1996 <ginokok1996@hotmail.com>
2020-10-14 14:38:09 +0200

    cancel reservations

--- api/src/Controller/ChinController.php
+++ api/src/Controller/ChinController.php
@@ -88,6 +88,25 @@ class ChinController extends AbstractController
             $person = $commonGroundService->getResource($this->getUser()->getPerson());
             $person = $commonGroundService->cleanUrl(['component' => 'cc', 'type' => 'people', 'id' => $person['id']]);
             $variables['reservations'] = $commonGroundService->getResourceList(['component' => 'arc', 'type' => 'reservations'], ['underName' => $person, 'order[dateCreated]' => 'desc'])['hydra:member'];
+
+            foreach ($variables['reservations'] as &$reservation) {
+                $nodes = $commonGroundService->getResourceList(['component' => 'chin', 'type' => 'nodes'],['accommodation' => $reservation['event']['calendar']['resource']])['hydra:member'];
+                if (count($nodes) > 0) {
+                    $reservation['node'] = $nodes[0];
+                }
+
+                if (isset($nodes[0]['configuration']['cancelable'])) {
+                    $hourDiff = round((strtotime('now') - strtotime($reservation['event']['startDate'])) / 3600);
+                    $dayDiff = round((strtotime($reservation['event']['startDate']) - strtotime('now')) / (60 * 60 * 24));
+
+
+                    if ($hourDiff < (float)$nodes[0]['configuration']['cancelable'] && $dayDiff == 0 ) {
+                        $reservation['cantCancel'] = true;
+                    }
+                }
+
+            }
+
         }
 
         return $variables;
@@ -1214,4 +1233,58 @@ class ChinController extends AbstractController
 
         return $variables;
     }
+
+    /**
+     * @Route("/cancel/{code}/{reservation}")
+     * @Template
+     */
+    public function cancelAction(Session $session, Request $request, CommonGroundService $commonGroundService, ApplicationService $applicationService, ParameterBagInterface $params, $code = null, $reservation = null)
+    {
+
+        // Fallback options of establishing
+        if (!$code) {
+            $code = $request->query->get('code');
+        }
+        if (!$code) {
+            $code = $request->request->get('code');
+        }
+        if (!$code) {
+            $code = $session->get('code');
+        }
+        if (!$code) {
+            $this->addFlash('warning', 'No node reference suplied');
+
+            return $this->redirect($this->generateUrl('app_default_index'));
+        }
+
+        $variables = [];
+
+        $session->set('code', $code);
+        $variables['code'] = $code;
+        $variables['resources'] = $commonGroundService->getResourceList(['component' => 'chin', 'type' => 'nodes'], ['reference' => $code])['hydra:member'];
+        if (count($variables['resources']) > 0) {
+            $variables['resource'] = $variables['resources'][0];
+        } else {
+            $this->addFlash('warning', 'Could not find a valid node for reference '.$code);
+
+            return $this->redirect($this->generateUrl('app_default_index'));
+        }
+
+        $variables['code'] = $code;
+        $variables['reservation'] = $commonGroundService->getResource(['component' => 'arc', 'type' => 'reservations', 'id' => $reservation]);
+
+        if ($request->isMethod('POST')) {
+            $reservation = $commonGroundService->getResource(['component' => 'arc', 'type' => 'reservations', 'id' => $request->get('reservationId')]);
+
+            $event = $reservation['event'];
+            $event['status'] = 'cancelled';
+            $event['calendar'] = '/calendars/'.$event['calendar']['id'];
+
+            $commonGroundService->updateResource($event);
+            $variables['cancelled'] = true;
+        }
+
+        return $variables;
+    }
+
 }
